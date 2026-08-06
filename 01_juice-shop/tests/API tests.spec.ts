b1cffa6f6e61a944223542cfc9ecd6264e0927ac
josenares <josejavier.naresj@gmail.com>
2026-08-06 17:18:44 -0600

    Update API tests.spec.ts

--- 01_juice-shop/tests/API tests.spec.ts
+++ 01_juice-shop/tests/API tests.spec.ts
@@ -4,6 +4,8 @@ import { Data_Dictionary } from '../Test_data/Page Object Definition';
 test('Log in', async ({ request,page }) => {
     const driver= new Data_Dictionary(page);
     const user_info = await driver.get_LoginInfo();
+    expect.soft(user_info[0]).not.toBeNull();
+    expect.soft(user_info[1]).not.toBeNull();
 
     const Login_response = await request.post('http://localhost:3000/rest/user/login',
     
@@ -14,15 +16,16 @@ test('Log in', async ({ request,page }) => {
             "password":user_info[1]
         }
     });
+    
 
-    expect (Login_response.status()).toEqual(200);
+    expect.soft(Login_response.status()).toEqual(200);
 
     const json = await Login_response.json()
     console.log(json);
 
-    expect(json).toHaveProperty("authentication.token")
-    expect(json.authentication.token).toBeTruthy();
-    expect(json).toHaveProperty('authentication.umail',user_info[0])
+    expect.soft(json).toHaveProperty("authentication.token")
+    expect.soft(json.authentication.token).toBeTruthy();
+    expect.soft(json).toHaveProperty('authentication.umail',user_info[0])
   
     
    
